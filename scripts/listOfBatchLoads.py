d1270af59b49b0433edd3c7a0abdc07b8bdbb295
ekman-kaur <ekman.kaur@pnnl.gov>
2024-04-10 14:26:29 -0700

    Update listOfBatchLoads script and create_batch script to include s3 pagination and appropriate logging

--- scripts/listOfBatchLoads.py
+++ scripts/listOfBatchLoads.py
@@ -1,18 +1,32 @@
 import boto3
+import logging
+
+logging.basicConfig(filename="error.log", level=logging.ERROR)
+
+
+def log_error(message):
+    logging.error(message)
+
 
 client = boto3.client("timestream-write", region_name="us-west-2")
 
 
 def count_batch_load_tasks_in_progress():
     try:
-        response = client.list_batch_load_tasks(TaskStatus="IN_PROGRESS")
+        response_in_progress = client.list_batch_load_tasks(TaskStatus="IN_PROGRESS")
+        response_created = client.list_batch_load_tasks(TaskStatus="CREATED")
+
+        num_in_progress = len(response_in_progress.get("BatchLoadTasks", []))
+        num_created = len(response_created.get("BatchLoadTasks", []))
+        num_total = num_in_progress + num_created
 
         temp = []
 
-        num_in_progress = len(response.get("BatchLoadTasks", []))
+        for task in response_in_progress.get("BatchLoadTasks", []):
+            table_name = task.get("TableName")
+            temp.append(table_name)
 
-        for task in response.get("BatchLoadTasks", []):
-            database_name = task.get("DatabaseName")
+        for task in response_created.get("BatchLoadTasks", []):
             table_name = task.get("TableName")
             temp.append(table_name)
 
@@ -23,21 +37,11 @@ def count_batch_load_tasks_in_progress():
             else:
                 table_counts[table] = 1
 
-        print("TABLE_COUNTS", table_counts)
+        logging.info("TABLE_COUNTS: %s", table_counts)
 
-        return num_in_progress, table_counts
+        return num_total, table_counts
 
-    except client.exceptions.InternalServerException as e:
-        print(f"Internal Server Error: {e}")
-    except client.exceptions.AccessDeniedException as e:
-        print(f"Access Denied: {e}")
-    except client.exceptions.ThrottlingException as e:
-        print(f"Throttling Error: {e}")
-    except client.exceptions.ValidationException as e:
-        print(f"Validation Error: {e}")
-    except client.exceptions.InvalidEndpointException as e:
-        print(f"Invalid Endpoint: {e}")
     except Exception as e:
-        print(f"An error occurred: {e}")
+        logging.exception("An error occurred: %s", e)
 
-    return 0, {}  # Return 0 if an error occurs
+    return 0, {}
